a7c885a00243f3e079992455bfd20837edde0615
reveghis <75759598+reveghis@users.noreply.github.com>
2023-10-09 15:44:47 +0200

    work on the decimating conquest
    Co-authored-by: Peyremorte Romain <Roropey@users.noreply.github.com>
    Co-authored-by: Anushree Shrivastava <anushree9699@users.noreply.github.com>

--- valence_driven_decimater.py
+++ valence_driven_decimater.py
@@ -4,6 +4,20 @@ import sys
 import random
 from collections import deque
 
+
+class Vertex_removed():
+    def __init__(self,index,valence,coordinates,index_vertex1_of_gate,index_vertex2_of_gate):
+        self.index = index
+        self.valence = valence
+        self.coordinates = coordinates
+        self.gate = [index_vertex1_of_gate,index_vertex2_of_gate]
+    def __init__(self,vertex,gate):
+        self.index = vertex.index
+        self.valence = len(vertex.faces)
+        self.coordinates = vertex.coordinates
+        self.gate = [gate[0].index,gate[1].index]
+
+        
 class Decimater(obja.Model):
     """
     A simple class that decimates a 3D model stupidly.
@@ -12,6 +26,7 @@ class Decimater(obja.Model):
         super().__init__()
         self.deleted_faces = set()
         self.gate = deque()
+        self.list_removed = []
     
     def find_the_gate(self, vertice_center,c_gate, init_gate = []):
         if init_gate != c_gate:
@@ -19,10 +34,10 @@ class Decimater(obja.Model):
                 init_gate = c_gate 
 
             for f in vertice_center.faces:
-                face = [f.a,f.b,f.c]
+                face = [self.vertices[f.a],self.vertices[f.b],self.vertices[f.c]]
                 if c_gate[0] in face and c_gate[1] in face and not vertice_center.index in face:
                     a = face.index(c_gate[1])
-                    nv_gate = [f[a],f[a+1]] 
+                    nv_gate = [self.vertices[f[a]],self.vertices[f[a+1]]] 
                     if nv_gate[0].state != 2 and nv_gate[1].state != 2 : 
                         nv_gate[0].state = 2 
                         nv_gate[1].state = 2
@@ -33,8 +48,12 @@ class Decimater(obja.Model):
 
     def find_the_gate_4_null_path(self, vertice_center,c_gate):
 
+        #Recherche of the gates for the case of null_path
+    
+        
+        # first I search the face corresponding to the gate and the vertice_center
         for f in vertice_center.faces:
-            face = [f.a,f.b,f.c]
+            face = [self.vertices[f.a],self.vertices[f.b],self.vertices[f.c]]
             if c_gate[0] in face and c_gate[1] in face and vertice_center.index in face:
                 nv_gate = [c_gate[1],vertice_center] 
                 if nv_gate[0].state != 2 and nv_gate[1].state != 2 : 
@@ -50,53 +69,66 @@ class Decimater(obja.Model):
 
 
 
+    def from_gate_to_face(self, gate_vertex1, gate_vertex2):
+        # Visit all faces from the 1st vertex to see if any face is shared in the right order with the 2nd vertex
+        # Return the face index and the three vertices of the face that have the gate
+        for index_face in gate_vertex1.faces:
+            if self.faces[index_face].a == gate_vertex1.index and self.face[index_face].b == gate_vertex2.index :
+                return [index_face,gate_vertex1,gate_vertex2,self.vertices[self.faces[index_face].c]]
+            elif self.face[index_face].b == gate_vertex1.index and self.face[index_face].c == gate_vertex2.index:
+                return [index_face,gate_vertex1,gate_vertex2,self.vertices[self.faces[index_face].a]]
+            elif self.face[index_face].c == gate_vertex1.index and self.face[index_face].a == gate_vertex2.index:
+                return [index_face,gate_vertex1,gate_vertex2,self.vertices[self.faces[index_face].b]]
+                
+        raise Exception("The two vertex given doesn't correspond to a gate.")
+
+    
 
 
-    def dacimateA(self, output):
+    
+    def decimating_conquest(self):
+
+        c_gate = self.gate.popleft()
+
+        # search for the front vertex
+        front_vertex = self.from_gate_to_face(c_gate[0], c_gate[1])[3]
         
-        print("ok")
-        # init
-        index_init = random.randint(0, len(self.faces))
+        
+        #if the front vertex is free and has a valence <= 6
+        if front_vertex.state == 1 and len(front_vertex.faces)<=6:
+            self.find_the_gate(front_vertex,c_gate)
+            front_vertex.state = 3
+            return Vertex_removed(front_vertex,c_gate)
+        
+        # else, (if its front vertex is free and has a valence > 6) or (if its front vertex is tagged conquered)
+        elif (front_vertex.state == 1 and len(front_vertex.faces)>6) or front_vertex.state == 2 :
+            self.find_the_gate_4_null_path(front_vertex,c_gate)
+            return None
+        
+        # else if the front face is tagged conquered or to be removed
+        else :
+            return None
+
+
+
+    def decimateA(self, output):
+        # inititialisation 
+        index_init = random.randint(0, len(self.faces)) # random index for faces
         faces_init = self.faces[index_init]
 
-        c_gate = [faces_init.a,faces_init.b]
-        for i in range (len(self.faces)) :
-            f = self.faces[i]
-            ptsf = [f.a,f.b,f.c]
-            if i != index_init and (faces_init.a in ptsf and faces_init.b in ptsf):
-                if not(f.a in c_gate) :
-                     vertice_center = f.a  
-                elif not(f.b in c_gate) :
-                    vertice_center = f.b 
-                else : 
-                    vertice_center = f.c
-
+        c_gate = [self.vertices[faces_init.a],self.vertices[faces_init.b]] # creation of the first gate
+        self.gate(c_gate)
         
-        if vertice_center.state == 1 and len(vertice_center.faces)<=6:
-
-            find_the_gate(self, vertice_center,c_gate, init_gate = [])
-
-
-            vertice_center.state == 3
-            for f in vertice_center.faces:
-                for i in [f.a,f.b,f.c]:
-                    if faces_init.c != i:
-                        self.vertices[i].state = 2
+        vertex_remove = self.decimating_conquest()
+        if vertex_remove != None:
+            # do the retriangulation
             
-
-            
-
-            output_val = len(vertice_center.faces)
         
-        elif (vertice_center.state == 1 and len(vertice_center.faces)>6) or vertice_center.state == 2 :
-            vertice_center.state == 2
+           
+"""
+    def retriangulation(current_gate)
 
-
-
-            output_val = "NullPatch"
-
-            
-
+"""
 
 
 
