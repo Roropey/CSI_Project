0ffc0e34ca8b0b6bc79f101e282e6dd3b027de35
reveghis <75759598+reveghis@users.noreply.github.com>
2023-10-31 12:30:29 +0100

    fonction cleaning_reconquest faites

--- reconstruction.py
+++ reconstruction.py
@@ -15,22 +15,22 @@ class Reconstructer(obja.Model):
 
     def decimating_reconquest(self,output_val):
         print("Start function decimating")
-        c_gate = self.gate.popleft()
         compte = len(output_val) - 1
         
-        
         while compte>=0:
+            c_gate = self.gate.popleft()
+
             # search for the front face information
             front_face_information = self.gate_to_face(c_gate[0], c_gate[1])
             front_face = self.faces[front_face_information[0]]
-            # if its front face is tagged conquered or to be removed
-            if front_face.state == obja.State.Conquered or front_face.state == obja.State.To_be_removed:
+
+            # if its front face is tagged conquered 
+            if front_face.state == obja.State.Conquered:
                 pass
 
             #elif  valence <= 6
             elif output_val[compte][0] in [3,4,5,6]:
-                # The front vertex is flagged to be removed and its incident faces are flagged to be removed.
-
+                
                 self.retriangulation(output_val[compte],c_gate)
                 front_face_information = self.gate_to_face(c_gate[0], c_gate[1])
                 front_vertex = front_face_information[3]
@@ -38,13 +38,10 @@ class Reconstructer(obja.Model):
                 # search for the gates and the front vertex neighboring vertices are flagged conquered
                 self.find_the_gate(front_vertex,c_gate)
 
-                front_vertex.state = obja.conquered
+                front_vertex.state = obja.State.Conquered 
                 for i in front_vertex.faces:
-                    self.faces[i].state = obja.conquered
+                    self.faces[i].state = obja.State.Conquered 
 
-                compte = compte - 1
-            
-            
             # else Null_patch
             elif output_val[compte] == "Null_patch":
                 # The front face is flagged conquered
@@ -59,16 +56,94 @@ class Reconstructer(obja.Model):
                     gate[1].state = obja.State.Conquered
                     self.gate.append(gate)
 
-                compte = compte - 1
-
-                #il faudrait voir comment implémenter le cas Null_patch dans vertex_removed
-                return "Null_patch"
             else:
-                raise Exception("Error in the decimating conquest")
+                raise Exception("Error in the output list of the decimating reconquest")
             
-            
-            
-        return None    
+            compte = compte - 1
+        
+        if compte>0 and len(self.gate)==0:
+            raise Exception("Error in the decimating reconquest")
+        
+
+    def cleaning_reconquest(self, output):
+
+        #function for reconstruction of cleaning-conquest
+        print("start the reconstruction of cleaning conquest:")
+        
+        compte = len(output)-1
+
+        while compte >=0 and len(self.gate)>0:
+            c_gate = self.gate.popleft()
+            # Find information about the front face
+            front_face_information = self.gate_to_face(c_gate[0], c_gate[1])
+            front_face = self.faces[front_face_information[0]]
+
+            # if its front face is tagged conquered
+            if front_face == obja.State.Conquered:
+                pass
+
+            elif output[compte][0] == 3:
+
+                self.retriangulation(output[compte],c_gate)
+
+                front_face_information = self.gate_to_face(c_gate[0], c_gate[1])
+                front_vertex = front_face_information[3]
+                front_face = self.faces[front_face_information[0]]
+
+                # find the edge of the patch
+                face_up_right = self.gate_to_face(front_vertex,c_gate[1])
+                face_up_left = self.gate_to_face(front_vertex, face_up_right[3])
+
+                # Mark the face conquered
+                self.faces[face_up_right[0]].state = obja.State.Conquered
+                self.faces[face_up_left[0]].state = obja.State.Conquered
+                front_face.state = obja.State.Conquered
+
+                # Create two intermediare gates
+                intermediaire_gates = [ [face_up_right[3], c_gate[1]] , [face_up_left[3], face_up_right[3]] ]
+
+                # find the other gates 
+                for gate in intermediaire_gates:
+                    gate[0].state = obja.State.Conquered
+                    gate[1].state = obja.State.Conquered
+                    f = self.gate_to_face(gate[0],gate[1])
+
+                    # Create the two new gates
+                    new_gates = [[f[3],f[2]], [f[1], f[3]]]
+
+                    # Add the new gates to the queue
+                    for gate in new_gates:
+                        gate[0].state = obja.State.Conquered
+                        gate[1].state = obja.State.Conquered
+                        self.gate.append(gate)
+                    self.faces[f[0]].state = obja.State.Conquered
+                
+                front_vertex.state = obja.State.Conquered
+
+
+            # else Null_patch
+            elif output[compte] == "Null_patch":
+                
+                # Mark the front face as conquered
+                front_face.state = obja.State.Conquered
+
+                # Create two new gates
+                new_gates = [[front_face_information[3],front_face_information[2]], [front_face_information[1],front_face_information[3]]]
+
+                # Add the new gates to the queue
+                for gate in new_gates:
+                    gate[0].state = obja.State.Conquered
+                    gate[1].state = obja.State.Conquered
+                    self.gate.append(gate)
+            else:
+                raise Exception("Error in the output list of cleaning reconquest")
+
+            compte = compte - 1
+
+        if compte>0 and len(self.gate)==0:
+            raise Exception("Error in the cleaning reconquest")
+
+  
 
     def recreate_faces(self,coordinates,border_patch,face_existing):
         # Create the vertex (on the model and the ouput)
@@ -170,7 +245,7 @@ class Reconstructer(obja.Model):
                 self.vertices[info_face_3[3].index].retriangulation_type = -1
                 border_patch.insert(3,info_face_2[3].index)
                 self.vertices[info_face_2[3].index].retriangulation_type = 1
-                border_patch.insert(4,info_face_4[3].index
+                border_patch.insert(4,info_face_4[3].index)
                 self.vertices[info_face_4[3].index].retriangulation_type = -1
 
 
@@ -178,86 +253,6 @@ class Reconstructer(obja.Model):
         self.recreate_faces(coordinates,border_patch,face_existing)
 
 
+    
 
-
-
-
-
-        
-
-        
-
-        
-
-
-    def cleaning_conquest(self, output):
-
-        #function for reconstruction of cleaning-conquest
-        print("start the reconstruction of cleaning conquest:")
-        c_gate = self.gate.popleft() 
-        compte = len(output)-1
-        vertex_valance = output[0]
-        vertex_index = output[1]
-
-        while compte >=0:
-            # Find information about the front face
-            front_face_information = self.gate_to_face(c_gate[0], c_gate[1])
-            front_vertex = front_face_information[3]
-            front_face = self.faces[front_face_information[0]]
-            #print("Front face state:{}".format(front_face.state))
-            # if its front face is tagged conquered or to be removed
-            if front_face == obja.State.Conquered or front_face == obja.State.To_be_removed:
-                return None
-
-
-            elif len(front_vertex.faces) == 3 and front_vertex.state == obja.State.Free :
-
-                # Mark the front face for removal
-                front_face.state = obja.State.To_be_removed
                 
-                # find the edge of the patch
-                face_up_right = self.gate_to_face(front_vertex,c_gate[1])
-                face_up_left = self.gate_to_face(front_vertex, face_up_right[3])
-
-                # Mark the other face for removal
-                self.faces[face_up_right[0]].state = obja.State.To_be_removed
-                self.faces[face_up_left[0]].state = obja.State.To_be_removed
-
-                # Create two intermediare gates
-                intermediaire_gates = [ [face_up_right[3], c_gate[1]] , [face_up_left[3], face_up_right[3]] ]
-
-                # find the other gates 
-                for gate in intermediaire_gates:
-                    gate[0].state = obja.State.Conquered
-                    gate[1].state = obja.State.Conquered
-                    f = self.gate_to_face(gate[0],gate[1])
-
-                    # Create the two new gates
-                    new_gates = [[f[3],f[2]], [f[1], f[3]]]
-
-                    # Add the new gates to the queue
-                    for gate in new_gates:
-                        gate[0].state = obja.State.Conquered
-                        gate[1].state = obja.State.Conquered
-                        self.gate.append(gate)
-                    self.faces[f[0]].state = obja.State.Conquered
-                
-                front_vertex.state = obja.State.To_be_removed
-
-                #return Vertex_removed(front_vertex,c_gate)
-
-            elif front_vertex.state == obja.State.Free or front_vertex == obja.State.Conquered :
-                
-                # Mark the front face as conquered
-                front_face.state = obja.State.Conquered
-
-                # Create two new gates
-                new_gates = [[front_face_information[3],front_face_information[2]], [front_face_information[1],front_face_information[3]]]
-
-                # Add the new gates to the queue
-                for gate in new_gates:
-                    gate[0].state = obja.State.Conquered
-                    gate[1].state = obja.State.Conquered
-                    self.gate.append(gate)
-
-                return "Null_patch"
